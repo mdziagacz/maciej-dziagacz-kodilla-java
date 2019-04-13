2a0705011dd5fe699f6e39fc134e152b7f19fe58
mohhio <m.dziagacz@gmail.com>
2019-04-13 20:32:36 +0200

    korekta kodu

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,15 +1,16 @@
 package com.kodilla.testing.collection;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class OddNumbersExterminator {
 
-    public ArrayList exterminate(ArrayList<Integer> numbers){
-        ArrayList<Integer> evenList = new ArrayList<Integer>() ;
+    public List<Integer> exterminate(List<Integer> numbers) {
+        List<Integer> evenList = new ArrayList<Integer>();
 
-        for (int i=0; i < numbers.size(); i++){
-            if(numbers.get(i) % 2 == 0){
-                evenList.add(numbers.get(i));
+        for (Integer temp : numbers) {
+            if (temp % 2 == 0) {
+                evenList.add(temp);
             }
         }
         return evenList;
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,51 +1,51 @@
 package com.kodilla.testing.collection;
 
 import org.junit.*;
+
 import java.util.*;
 
 public class CollectionTestSuite {
 
     @After
-    public void after(){
+    public void after() {
         System.out.println("test zakończony");
     }
 
     @Before
-    public void before(){
-        System.out.print("rozpoczynam test ");
+    public void before() {
+        System.out.println("rozpoczynam test ");
     }
 
     @Test
-    public void testOddNumbersExterminatorNormalList(){
-        System.out.println("na parzyste");
+    public void testOddNumbersExterminatorNormalList() {
         //Given
-        ArrayList<Integer> testList = new ArrayList<>();
-        ArrayList<Integer> exterminatedList = new ArrayList<>();
+        List<Integer> testList = new ArrayList<>();
         List<Integer> expectedList = new ArrayList<>();
         OddNumbersExterminator exterminator = new OddNumbersExterminator();
-        boolean test;
         testList.add(3);
         testList.add(6);
         testList.add(9);
         testList.add(2);
         expectedList.add(6);
         expectedList.add(2);
+
         //When
-        exterminatedList = exterminator.exterminate(testList);
+        List<Integer> exterminatedList = exterminator.exterminate(testList);
+
         //Then
         Assert.assertEquals(expectedList, exterminatedList);
     }
 
     @Test
-    public void testOddNumbersExterminatorEmptyList(){
-        System.out.println("na pustej liście");
+    public void testOddNumbersExterminatorEmptyList() {
         //Given
-        ArrayList<Integer> testList = new ArrayList<>();
-        ArrayList<Integer> exterminatedList = new ArrayList<>();
+        List<Integer> testList = new ArrayList<>();
         OddNumbersExterminator exterminator = new OddNumbersExterminator();
+
         //When
-        exterminatedList = exterminator.exterminate(testList);
+        List<Integer> exterminatedList = exterminator.exterminate(testList);
+
         //Then
-        Assert.assertTrue(exterminatedList.size() == 0);
+        Assert.assertTrue(exterminatedList.isEmpty());
     }
 }
