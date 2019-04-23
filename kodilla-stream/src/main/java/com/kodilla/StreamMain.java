7379daba3e4a581bcad3c21aa01da9966660fa19
mohhio <m.dziagacz@gmail.com>
2019-04-23 20:26:39 +0200

    add missing kodilla-testing

---
+++ kodilla-stream/src/main/java/com/kodilla/StreamMain.java
@@ -0,0 +1,32 @@
+package com.kodilla;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, (a, b) -> FunctionalCalculator.multiplyAByB(a, b));
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+//task
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        System.out.println("text to decorate: lamdba is cool");
+        System.out.println("decorated text:");
+        String text = "lambda is cool";
+        poemBeautifier.beautify(text, textToDecorate -> "*" + textToDecorate);
+
+    }
+}
