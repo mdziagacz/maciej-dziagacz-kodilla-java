684508b14282b6b004b2f1c32c34053759937c98
mohhio <m.dziagacz@gmail.com>
2019-04-24 22:16:58 +0200

    stream 7.3 - lesson

--- kodilla-stream/src/main/java/com/kodilla/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/StreamMain.java
@@ -2,9 +2,16 @@ package com.kodilla;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.FunctionalCalculator;
+import com.kodilla.stream.person.People;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -22,7 +29,7 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
-//task
+//task lambda 7.1
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         System.out.println("text to decorate: lamdba is cool");
         System.out.println("decorated text:");
@@ -33,5 +40,24 @@ public class StreamMain {
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
+
+// Stream 7.3
+        People.getList().stream()
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .map(s -> s.toUpperCase())
+                .filter(s -> s.substring(0,1).equals("M"))
+                .forEach(System.out::println);
+
+        BookDirectory bookDirectory = new BookDirectory();
+            BookDirectory theBookDirectory = new BookDirectory();
+            String theResultStringOfBooks = theBookDirectory.getList().stream()
+                    .filter(book -> book.getYearOfPublication() > 2005)
+                    .map(Book::toString)
+                    .collect(Collectors.joining(",\n","<<",">>"));
+
+            System.out.println(theResultStringOfBooks);
+
+
     }
 }
