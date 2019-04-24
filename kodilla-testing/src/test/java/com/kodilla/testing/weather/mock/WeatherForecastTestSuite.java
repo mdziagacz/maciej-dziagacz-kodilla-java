88cda4a4730e58fb729bfcf577e9a8e260093d98
mohhio <m.dziagacz@gmail.com>
2019-04-24 20:21:00 +0200

    add lost classes

--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -7,7 +7,9 @@ import org.junit.Test;
 
 import java.util.HashMap;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 public class WeatherForecastTestSuite {
     @Test
