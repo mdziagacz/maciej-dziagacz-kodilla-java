8c17413c7f56b27a4ced2167f670e76d45a2f263
mohhio <m.dziagacz@gmail.com>
2019-04-27 21:35:10 +0200

    without if (.orElse used)

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -5,9 +5,6 @@ import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
-        if (numbers.length == 0){
-            return 0;
-        }
         IntStream.range(0, numbers.length)
                 .forEach(n -> System.out.println(numbers[n]));
 
