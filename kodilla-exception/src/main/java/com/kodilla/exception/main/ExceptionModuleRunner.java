de7640700ba1a2096b3c7cab4922ae49fbb8ba01
mohhio <m.dziagacz@gmail.com>
2019-05-01 15:50:25 +0200

    8.3 eceptions - lesson

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,10 +1,15 @@
 package com.kodilla.exception.main;
 
-import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
 
 public class ExceptionModuleRunner {
     public static void main(String[] args) {
-        FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        FileReaderWithoutHandling fileReader = new FileReaderWithoutHandling();
+        try{
+            fileReader.readFile();
+        }catch (FileReaderException e){
+            System.out.println("can't open the file");
+        }
     }
 }
