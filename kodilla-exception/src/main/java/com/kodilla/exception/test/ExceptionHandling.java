710dcca6e116e4f7d4cde71b6a13fbb29e014526
mohhio <m.dziagacz@gmail.com>
2019-05-03 12:04:45 +0200

    8.3 exception - task after comments

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,13 @@
 package com.kodilla.exception.test;
 
-public class ExceptionHandling extends Exception {
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge test = new SecondChallenge();
+
+        try{
+            test.probablyIWillThrowException(3, 1);
+        }catch (Exception e){
+            System.out.println("Exception catched");
+        }
+    }
 }
