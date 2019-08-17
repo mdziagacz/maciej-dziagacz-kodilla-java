234dfe8613b223843f1ea4b77be248a5d1d3b508
mohhio <m.dziagacz@gmail.com>
2019-08-17 14:18:02 +0200

    23.1 loop for debugger

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testLoopAndConditionalBreakpoints {
+    @Test
+    public void testLoop(){
+        //Given
+        long sum = 0;
+
+        //When
+        for (int i = 0; i < 1000; i ++){
+            sum +=i;
+            System.out.println("[ " + i + " ], sum equals: " + sum);
+        }
+
+        //Then
+        Assert.assertEquals(499500, sum);
+    }
+
+}
